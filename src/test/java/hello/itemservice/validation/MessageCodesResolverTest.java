bcab15699cc41f4c9bd10733414d2c8f00035295
이승환 <seaung13@gmail.com>
2025-01-05 15:03:39 +0900

    test: MessageCodesResolver 테스트

---
+++ src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -0,0 +1,33 @@
+package hello.itemservice.validation;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MessageCodesResolverTest {
+
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @DisplayName("MessageCodesResolver 로 에러 메세지 코드를 생성할 수 있다.")
+    @Test
+    void createMessageCodes() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+        assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+
+    @DisplayName("MessageCodesResolver 로 에러 메세지 코드를 생성할 수 있다. 2")
+    @Test
+    void createMessageCodes2() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        assertThat(messageCodes).containsExactly(
+                "required.item.itemName",
+                "required.itemName",
+                "required.java.lang.String",
+                "required"
+        );
+    }
+
+}

34e78fa19193263d10d3b8df11d556d6acc74722
이승환 <seaung13@gmail.com>
2025-01-05 16:48:22 +0900

    test: Bean Validation 기술을 이용한 Item 등록 입력 값 검증

---
+++ src/test/java/hello/itemservice/validation/BeanValidationTest.java
@@ -0,0 +1,38 @@
+package hello.itemservice.validation;
+
+import hello.itemservice.domain.item.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+public class BeanValidationTest {
+
+    @DisplayName("BeanValidation")
+    @Test
+    void beanValidation() {
+        // given
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        Item item = new Item();
+        item.setItemName(" ");
+        item.setPrice(0);
+        item.setQuantity(10000);
+
+        Set<ConstraintViolation<Item>> violations = validator.validate(item);
+        for (ConstraintViolation<Item> violation : violations) {
+            System.out.println("violation = " + violation);
+            System.out.println("violation.message = " + violation.getMessage());
+
+        }
+        // when
+
+        // then
+
+    }
+}
